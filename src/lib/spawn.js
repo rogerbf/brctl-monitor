21300159dc8a3c19576ad9661c699c37f9aaa871
rogerbf <roger@bergfalk.nu>
2016-09-25 18:56:16 +0200

    resolve on readable

--- src/lib/spawn.js
+++ src/lib/spawn.js
@@ -11,23 +11,24 @@ const defaultOptions = {
 const spawnInstance = userOptions => {
   const options = Object.assign({}, defaultOptions, userOptions)
   const baseDirectory = path.join(os.homedir(), `Library/Mobile Documents/`)
+
   const directory = options.directory
   ? path.join(baseDirectory, `/com~apple~CloudDocs`, options.directory)
   : null
+
   const args = directory
   ? [`monitor`, `--scope=${options.scope.toUpperCase()}`, directory]
   : [`monitor`, `--scope=${options.scope.toUpperCase()}`]
+
   return new Promise((resolve, reject) => {
     const instance = spawn(`brctl`, args)
     instance.on(`error`, err => reject(err))
-    if (typeof(instance.pid) === `number`) {
-      resolve(instance)
-    } else { reject(`brctl did not spawn correctly`) }
+    instance.stdout.once(`readable`, () => resolve(instance))
   })
 }
 
 module.exports = spawnInstance
 
-// spawnInstance({ directory: `test` })
-//   .then(instance => instance.stdout.on(`data`, data => console.log(data.toString())))
-//   .catch(e => console.log(e.toString()))
+spawnInstance({ directory: `test` })
+  .then(instance => instance.stdout.on(`data`, data => console.log(data.toString())))
+  .catch(e => console.log(e.toString()))
