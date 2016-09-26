434d0be5c78e179e76ef52971bd03e28f66a5438
rogerbf <roger@bergfalk.nu>
2016-09-26 15:07:13 +0200

    parseState

--- src/lib/parseState.js
+++ src/lib/parseState.js
@@ -1,13 +1,6 @@
 const Transform = require(`stream`).Transform
 const objectFromString = require(`object-from-string`)(`:`)
 
-// const parseLines = chunk => {
-//   const chunkString = chunk.toString()
-//   const dateIndex = chunkString.match(/\d{4}-\d{2}-\d{2}/).index
-//   const lastNewline = chunkString.lastIndexOf(`\n\n`)
-//   return chunkString.slice(dateIndex, lastNewline).split(`\n`)
-// }
-
 const parseStatusLine = line => {
   const timeStatusDivider = line.match(/\+\d{4}/).index + 5
   const timestamp = line.slice(0, timeStatusDivider).trim()
@@ -19,19 +12,19 @@ const parseStatusLine = line => {
   return { timestamp, status }
 }
 
-const parseFilesArray = filesString => {
+const parseFiles = filesString => {
   return { files: filesString.split(`\n o `).slice(1) }
 }
 
 const parseState = new Transform({
   transform(chunk, encoding, callback) {
-    // const lines = parseLines(chunk)
     const chunkString = chunk.toString()
+    const filesDivider = chunkString.match(/\n\so\s/).index
     this.push(
       Object.assign(
         {},
-        parseStatusLine(chunkString.slice(chunkString.indexOf(`\n`), chunkString.match(/\n\so\s/).index)),
-        parseFilesArray(chunkString.slice(chunkString.match(/\n\so\s/).index))
+        parseStatusLine(chunkString.slice(chunkString.match(/\d{4}-\d{2}-\d{2}/).index, filesDivider)),
+        parseFiles(chunkString.slice(filesDivider))
       )
     )
     callback()
