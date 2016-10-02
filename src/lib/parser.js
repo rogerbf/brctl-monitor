b4de6f8fb1d6f38ac39d5c2fee5fa6ca080d2c1c
rogerbf <roger@bergfalk.nu>
2016-10-02 22:52:30 +0200

    parser, tests

--- src/lib/parser.js
+++ src/lib/parser.js
@@ -1,7 +1,17 @@
 const Transform = require(`stream`).Transform
+const isolateSections = require(`./isolateSections.js`)
+const decomposeHead = require(`./decomposeHead.js`)
+const separateFiles = require(`./separateFiles.js`)
+const decomposeFile = require(`./decomposeFile.js`)
 
 module.exports = new Transform({
   transform(chunk, encoding, next) {
+    const cstr = chunk.toString()
+    const sections = isolateSections(cstr)
+    const head = decomposeHead(sections.head)
+    const files = separateFiles(sections.files).map(decomposeFile)
+    this.push(Object.assign({}, head, files))
+    next()
   },
   objectMode: true
 })
--- src/tests/parser.test.js
+++ src/tests/parser.test.js
@@ -5,3 +5,32 @@ test(`is a Transform stream`, assert => {
   assert.ok(parser.constructor.name === `Transform`)
   assert.end()
 })
+
+test(`return expected data`, assert => {
+  const data = (
+    `observing in /Users/roger/Library/Mobile Documents/com~apple~CloudDocs/temp for the docs|data|external scope(s)\n` +
+    `2016-09-28 09:18:40 +0000 total:4\n` +
+    ` o /randomdump ☁\n` +
+    ` o /test 2 ☁\n` +
+    ` o /testfile3 ☁\n` +
+    ` o /test1.md ☁\n` +
+    `\n` +
+    `2016-09-28 09:18:40 +0000 gathering done in 0.003s\n` +
+    `\n`
+  )
+
+  const expected = {
+    timestamp: `2016-09-28 09:18:40 +0000`,
+    stats: {
+      total: 4
+    }
+  }
+
+  parser.on(`data`, parsedData => {
+    assert.equal(parsedData.timestamp, expected.timestamp)
+    assert.deepEqual(parsedData.stats, expected.stats)
+    assert.end()
+  })
+
+  parser.write(data)
+})
