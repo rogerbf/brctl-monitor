2cb9b6c809ff1ee4e8f4bd523dd6e1de764d2192
rogerbf <roger@bergfalk.nu>
2016-10-03 17:49:13 +0200

    rewrite, remove misguided tests

--- src/lib/consolidateChunks.js
+++ src/lib/consolidateChunks.js
@@ -1,20 +1,35 @@
 const Transform = require(`stream`).Transform
 const split = require(`recursive-buffer-split`)
 
-const newline_newline = Buffer(`\n\n`)
+const newline = Buffer(`\n`)
 
 module.exports = () => {
   return Object.assign(
     new Transform({
       transform(chunk, encoding, next) {
-        split(newline_newline, chunk)
-          .map(s => {
-            if (s.length > 0) {
-              this.push(Buffer.concat([s, newline_newline]))
+
+        const arr = split(newline, chunk)
+
+          const processed = arr.reduce((acc, c) => {
+            if (c.indexOf(` o `) >= 0 || c.indexOf(`total:`) >= 0) {
+              return Buffer.concat([ acc, c, Buffer(`\n`) ])
+            } else {
+              return acc
             }
-          })
+          }, this.queue)
+
+          this.queue = Buffer.alloc(0)
+
+          if (arr[arr.length - 1].length === 0 && arr[arr.length - 2].length === 0) {
+            this.push(processed)
+            this.queue = Buffer.alloc(0)
+          } else {
+            this.queue = Buffer.concat([ this.queue, processed ])
+          }
+
         next()
       }
-    })
+    }),
+    { queue: Buffer.alloc(0) }
   )
 }
