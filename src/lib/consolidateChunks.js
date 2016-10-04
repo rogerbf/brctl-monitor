56ac3ce5bd9ee3d551b626fa2288048e418c8904
rogerbf <roger@bergfalk.nu>
2016-10-04 14:54:30 +0200

    new tests

--- src/lib/consolidateChunks.js
+++ src/lib/consolidateChunks.js
@@ -3,33 +3,33 @@ const split = require(`recursive-buffer-split`)
 
 const newline = Buffer(`\n`)
 
-module.exports = () => {
-  return Object.assign(
+module.exports = () =>
+  Object.assign(
     new Transform({
       transform(chunk, encoding, next) {
 
         const arr = split(newline, chunk)
 
-          const processed = arr.reduce((acc, c) => {
-            if (c.indexOf(` o `) >= 0 || c.indexOf(`total:`) >= 0) {
-              return Buffer.concat([ acc, c, Buffer(`\n`) ])
-            } else {
-              return acc
-            }
-          }, this.queue)
-
-          this.queue = Buffer.alloc(0)
-
-          if (arr[arr.length - 1].length === 0 && arr[arr.length - 2].length === 0) {
-            this.push(processed)
-            this.queue = Buffer.alloc(0)
+        const processed = arr.reduce((acc, c) => {
+          if (c.indexOf(` o `) >= 0 || c.indexOf(`total:`) >= 0) {
+            return Buffer.concat([acc, c, Buffer(`\n`)])
           } else {
-            this.queue = Buffer.concat([ this.queue, processed ])
+            return acc
           }
+        }, this.queue)
+
+        this.queue = Buffer.alloc(0)
+
+        if (arr[arr.length - 1].length === 0 && arr[arr.length - 2].length === 0) {
+          this.push(processed)
+          this.queue = Buffer.alloc(0)
+        } else {
+          this.queue = Buffer.concat([this.queue, processed])
+        }
 
         next()
       }
-    }),
-    { queue: Buffer.alloc(0) }
+    }), {
+      queue: Buffer.alloc(0)
+    }
   )
-}
--- src/tests/consolidateChunks.test.js
+++ src/tests/consolidateChunks.test.js
@@ -1,14 +1,64 @@
 import test from 'tape'
 import consolidateChunks from '../lib/consolidateChunks.js'
 
-test(`is a function`, assert => {
-  assert.ok(typeof(consolidateChunks) === `function`)
+const createInstance = () => {
+  return consolidateChunks()
+}
+
+test(`is a transform stream`, assert => {
+  const instance = createInstance()
+  assert.equal(instance.constructor.name, `Transform`)
   assert.end()
 })
 
-const transform = consolidateChunks()
+test(`expected output #1`, assert => {
+  const data = (
+    `observing in /Users/roger/Library/Mobile Documents/com~apple~CloudDocs/temp for the docs|data|external scope(s)\n` +
+    `2016-09-28 08:25:43 +0000 total:0\n` +
+    `\n` +
+    `2016-09-28 08:25:43 +0000 gathering done in 0.002s\n` +
+    `\n`
+  )
 
-test(`returns a transform stream`, assert => {
-  assert.equal(transform.constructor.name, `Transform`)
-  assert.end()
+  const expected = `2016-09-28 08:25:43 +0000 total:0\n`
+
+  const instance = createInstance()
+
+  instance.on(`data`, data => {
+    const actual = data.toString()
+    assert.equal(actual, expected)
+    assert.end()
+  })
+
+  instance.write(data)
+})
+
+test(`expected output #2`, assert => {
+  const data = {
+    a: `observing in /Users/roger/Library/Mobile Documents/com~apple~CloudDocs/temp for the docs|data|external scope(s)\n` +
+      `2016-09-28 08:25:43 +0000 total:0\n`,
+    b: `\n`,
+    c: `2016-09-28 08:25:43 +0000 gathering done in 0.002s\n` +
+      `\n`
+  }
+
+  const expected = `2016-09-28 08:25:43 +0000 total:0\n`
+
+  let actual = ``
+
+  const instance = createInstance()
+
+  instance.on(`data`, data => {
+    actual = actual.concat(data.toString())
+  })
+
+  instance.once(`finish`, () => {
+    assert.equal(actual, expected)
+    assert.end()
+  })
+
+  instance.write(data.a)
+  instance.write(data.b)
+  instance.write(data.c)
+  instance.end()
 })
